c5ee82b41f05dfea898c30fb1af20bdf04e92660
pthfinder <60949482+pthfinder@users.noreply.github.com>
2020-11-21 07:57:07 -0800

    Building actual combat events.

---
+++ prowess.java
@@ -0,0 +1,7 @@
+public class prowess {
+    public static void main(String [] args) {
+        for (int prowessValue = 0; prowessValue <= 100; prowessValue++) {
+            System.out.println("If = { \n limit = {\n prowess > " + prowessValue + "\n } \n add = " + prowessValue + "\n }");
+        }
+    }
+}
